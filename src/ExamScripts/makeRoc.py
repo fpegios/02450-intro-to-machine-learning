986d58a8ffa386ae6a410061fd2f79e0c3d4eaba
Fotis Pegios <fotispeg@gmail.com>
2018-05-24 16:55:53 +0200

    remove files

--- src/ExamScripts/makeRoc.py
+++
@@ -1,9 +0,0 @@
-from sklearn import metrics
-import matplotlib.pyplot as plt
-
-trueLabels = [1,1,0,1,0,0]
-predicted = [1,1,1,0,0,0]
-fpr, tpr, _ = metrics.roc_curve(trueLabels,predicted)
-plt.plot(fpr,tpr)
-
-print("AUC = ",metrics.roc_auc_score(trueLabels,predicted))
